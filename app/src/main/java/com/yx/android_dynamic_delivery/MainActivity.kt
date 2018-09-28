7e7aa0ede1779b0a9f68e2c01558ff66289e03d7
xiao.yang <developer.yx@gmail.com>
2018-09-28 15:04:08 +0800

    init

---
+++ app/src/main/java/com/yx/android_dynamic_delivery/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yx.android_dynamic_delivery
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
